1b2cd5ec7b231f9a829ee4acee01db8842116d34
unknown <Administrateur@138SE8-6T92F53.ad.campus-eni.fr>
2025-03-20 10:35:11 +0100

    #3 S003 Couche DAL JPA - Gérer l'entité Client

---
+++ src/main/java/fr/eni/ludotheque/bo/Client.java
@@ -0,0 +1,30 @@
+package fr.eni.ludotheque.bo;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "CLIENTS")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @Column(nullable = false, length = 50)
+    @NonNull private String nom;
+
+    @Column(nullable = false, length = 50)
+    @NonNull private String prenom;
+
+    @Column(nullable = false, length = 10)
+    @NonNull private String telephone;
+
+    @Column(nullable = false, length = 50)
+    @NonNull private String email;
+}
